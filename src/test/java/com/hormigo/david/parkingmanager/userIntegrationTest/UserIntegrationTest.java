23ca51a4812f54df460828730e087960f2be50bb
PabloJavier22 <117372943+PabloJavier22@users.noreply.github.com>
2023-05-04 15:19:55 +0200

    hola

---
+++ src/test/java/com/hormigo/david/parkingmanager/userIntegrationTest/UserIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.hormigo.david.parkingmanager.userIntegrationTest;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.hormigo.david.parkingmanager.user.service.UserService;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserIntegrationTest {
+
+    @MockBean
+    private UserService userService;
+    @Value("${local.server.port}")
+    private  int port;
+    private static ChromeDriver chromeDriver;
+    @BeforeAll
+    public static void prepareWebDriver() {
+
+        System.setProperty("webdriver.chrome.driver","D:\\ChromeDriver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        chromeDriver = new ChromeDriver(options);
+        
+    }
+}
